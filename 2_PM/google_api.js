557a8a2aa0605123454b81b0eff1ac4c5e396b70
Mohamed Ally Iddi <mohamed_iddi@college.harvard.edu>
2019-01-08 16:12:40 -0500

    Successful request and correct message to user

--- 2_PM/google_api.js
+++ 2_PM/google_api.js
@@ -1,1 +1,9 @@
 // TODO - create client object for Google API connection which can be exported
+
+const config = require("./config");
+const googleMapsClient = require("@google/maps").createClient({
+  key: config.API_KEY,
+  Promise: Promise
+});
+
+module.exports = googleMapsClient;
--- 2_PM/placeinfo.js
+++ 2_PM/placeinfo.js
@@ -2,3 +2,39 @@
 / returns the following string, formatted with the appropriate values:
 
 'INPUTTED_PLACE is located at coordinates RETURNED_COORDINATES, at the address RETURNED_ADDRESS.' */
+const googleMapsClient = require("./google_api");
+
+const lookupPlace = process.argv[2];
+
+if (!lookupPlace) {
+  console.log("Usage: node placeinfo.js PLACENAME");
+  return -1;
+}
+
+googleMapsClient
+  .findPlace({
+    input: lookupPlace,
+    inputtype: "textquery"
+  })
+  .asPromise()
+  .then(res => {
+    let candidates = res.json.candidates;
+    candidates.forEach(candidate => {
+      googleMapsClient
+        .place({
+          placeid: candidate.place_id
+        })
+        .asPromise()
+        .then(res => {
+          let coordinates = res.json.result.geometry.location;
+          let address = res.json.result.formatted_address;
+          console.log(
+            `${lookupPlace} is located at coordinates Latitude: ${
+              coordinates.lat
+            }, Longitude: ${coordinates.lng}, at the address ${address}`
+          );
+        })
+        .catch(err => console.log(err));
+    });
+  })
+  .catch(err => console.log(err));
